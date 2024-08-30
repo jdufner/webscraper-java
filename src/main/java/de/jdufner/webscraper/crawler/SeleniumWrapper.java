eff0d43fe2943374099c6ee2501e00371e57ff25
Jürgen Dufner <juergen@jdufner.de>
2024-08-30 11:15:43 +0200

    Refactor scrollDownPageByPage

--- src/main/java/de/jdufner/webscraper/crawler/SeleniumWrapper.java
+++ src/main/java/de/jdufner/webscraper/crawler/SeleniumWrapper.java
@@ -41,25 +41,25 @@ public class SeleniumWrapper {
     private void scrollDownPageByPage() throws Exception {
         long innerHeight = getInnerHeight();
         long scrollHeight = getScrollHeight();
-        long pageYOffset = getPageYOffset();
-        long scrollY = getScrollY();
-        long scrollPosition = 0;
         int index = 1;
-//        logger.info("innerHeight = {}, scrollHeight = {}, scrollPosition = {}", innerHeight, scrollHeight, scrollPosition);
-        while (scrollPosition < (long)(.99 * (scrollHeight - innerHeight)) && index <= 100) {
-            scrollToPosition(index * innerHeight);
-            Thread.sleep(1000);
-            scrollPosition = getScrollPosition();
+        while (getPixelsBelowWindow() >= 0.9 * innerHeight && index <= 100) {
+            logger.debug("innerHeight = {}, scrollHeight = {}, index = {}, scrollY = {}", innerHeight, scrollHeight, index, getScrollY());
+            scrollVerticallyBy(innerHeight);
+            index += 1;
+            Thread.sleep(500);
             long newScrollHeight = getScrollHeight();
-//            logger.info("scrollPosition = {}, newScrollHeight = {}", scrollPosition, newScrollHeight);
             if (newScrollHeight > scrollHeight) {
-                index = -1;
+                index = 1;
                 scrollHeight = newScrollHeight;
             }
-            index += 1;
 
         }
         scrollToEndOfPage();
+    }
+
+    private long getPixelsBelowWindow() {
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
+        return ((Number) javascriptExecutor.executeScript("return document.body.scrollHeight - window.scrollY - window.innerHeight")).longValue();
     }
 
     private long getScrollY() {
@@ -67,24 +67,14 @@ public class SeleniumWrapper {
         return (Long) javascriptExecutor.executeScript("return window.scrollY");
     }
 
-    private long getPageYOffset() {
+    private void scrollVerticallyBy(long verticalPixels) throws Exception {
         JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
-        return (Long) javascriptExecutor.executeScript("return window.pageYOffset");
-    }
-
-    private void scrollToPosition(long position) throws Exception {
-        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
-        javascriptExecutor.executeScript("window.scrollBy(0, " + position + ")");
+        javascriptExecutor.executeScript("window.scrollBy(0, " + verticalPixels + ")");
     }
 
     private void scrollToEndOfPage() {
         JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
         javascriptExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-    }
-
-    private long getScrollPosition() {
-        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
-        return (Long) javascriptExecutor.executeScript("return window.pageYOffset + window.innerHeight");
     }
 
     private long getScrollHeight() {
