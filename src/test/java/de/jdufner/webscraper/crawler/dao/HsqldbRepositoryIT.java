cc5bfe76a216d462ed821edae30d6ea316ca9443
Jürgen Dufner <juergen@jdufner.de>
2024-11-15 15:25:07 +0100

    increase test coverage

--- src/test/java/de/jdufner/webscraper/crawler/dao/HsqldbRepositoryIT.java
+++ src/test/java/de/jdufner/webscraper/crawler/dao/HsqldbRepositoryIT.java
@@ -2,13 +2,16 @@ package de.jdufner.webscraper.crawler.dao;
 
 import de.jdufner.webscraper.crawler.data.HtmlPage;
 import de.jdufner.webscraper.crawler.data.Image;
+import de.jdufner.webscraper.crawler.data.Link;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.io.File;
 import java.net.URI;
 import java.util.Date;
+import java.util.Objects;
 import java.util.Optional;
 
 import static java.util.Arrays.asList;
@@ -53,7 +56,7 @@ class HsqldbRepositoryIT {
     }
 
     @Test
-    public void when_get_next_image_expect_image() {
+    public void given_at_least_one_image_in_database_when_get_next_image_expect_image() {
         // arrange
         jdbcTemplate.update("insert into IMAGES (URL) values (?)", "https://www.google.com/image.jpg");
 
@@ -66,7 +69,7 @@ class HsqldbRepositoryIT {
     }
 
     @Test
-    public void when_get_no_images_available_expect_null() {
+    public void given_no_images_in_database_when_get_no_images_available_expect_empty() {
         // arrange
         jdbcTemplate.update("delete from IMAGES");
 
@@ -75,6 +78,69 @@ class HsqldbRepositoryIT {
 
         // assert
         assertThat(image.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void given_image_when_image_exists_expect_updated() {
+        // arrange
+        Image image = new Image(-1, URI.create("http://localhost/image.jpg"));
+        jdbcTemplate.update("insert into IMAGES (ID, URL) values (?, ?)", image.id(), image.uri().toString());
+        File file = new File("image.jpg");
+
+        // act
+        hsqldbRepository.setImageDownloadedAndFilename(image, file);
+
+        // assert
+        Object[] data = Objects.requireNonNull(jdbcTemplate.queryForObject(
+                "select filename, downloaded from IMAGES where id = ?",
+                (rs, rowNum) ->  new Object[]{rs.getString("filename"), rs.getBoolean("downloaded")},
+                image.id()
+        ));
+        assertThat(data[0]).isEqualTo(file.getPath());
+        assertThat(data[1]).isEqualTo(Boolean.TRUE);
+    }
+
+    @Test
+    public void given_at_least_one_link_in_database_when_get_next_link_expect_link() {
+        // arrange
+        jdbcTemplate.update("insert into LINKS (URL) values (?)", "https://www.google.com/");
+
+        // act
+        Optional<Link> link = hsqldbRepository.getNextLinkIfAvailable();
+
+        // assert
+        assertThat(link.isPresent()).isTrue();
+        assertThat(link.get().uri()).isEqualTo(URI.create("https://www.google.com/"));
+    }
+
+    @Test
+    public void given_no_link_in_database_when_get_next_link_expect_empty() {
+        // arrange
+        jdbcTemplate.update("delete from LINKS");
+
+        // act
+        Optional<Link> link = hsqldbRepository.getNextLinkIfAvailable();
+
+        // assert
+        assertThat(link.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void given_link_when_link_exists_expect_updated() {
+        // arrange
+        Link link = new Link(-1, URI.create("https://localhost/"));
+        jdbcTemplate.update("insert into LINKS (ID, URL) values (?, ?)", link.id(), link.uri().toString());
+
+        // act
+        hsqldbRepository.setLinkDownloaded(link);
+
+        // assert
+        Boolean downloaded = jdbcTemplate.queryForObject(
+                "select DOWNLOADED from LINKS where id = ?",
+                (rs, rowNum) -> rs.getBoolean("DOWNLOADED"),
+                link.id()
+        );
+        assertThat(downloaded).isTrue();
     }
 
 }
