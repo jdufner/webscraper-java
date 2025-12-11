0cd619d069d98f4d5bc21ad49f972f604e33a136
Jürgen Dufner <847334+jdufner@users.noreply.github.com>
2025-12-11 10:25:15 +0100

    add test for selectTwoRandomPictures

--- src/main/java/de/jdufner/webscraper/picturechoice/PictureSelector.java
+++ src/main/java/de/jdufner/webscraper/picturechoice/PictureSelector.java
@@ -17,7 +17,7 @@ class PictureSelector {
         int totalNumberPictures = pictureRepository.totalNumberPictures();
         int picture1Index = randInt(1,  totalNumberPictures);
         Picture picture1 = pictureRepository.loadPictureOrNextAfter(picture1Index);
-        int picture2Index = randIntExclude(1,  totalNumberPictures, picture1Index);
+        int picture2Index = randInt(1,  totalNumberPictures, picture1Index);
         Picture picture2 = pictureRepository.loadPictureOrNextAfter(picture2Index);
         return new Picture[]{picture1, picture2};
     }
@@ -26,7 +26,7 @@ class PictureSelector {
         return (int) (Math.random() * (max - min + 1)) + min;
     }
 
-    static int randIntExclude(int min, int max, int exclude) {
+    static int randInt(int min, int max, int exclude) {
         int candidate = randInt(min, max);
         while (candidate == exclude) {
             candidate = randInt(min, max);
--- src/test/java/de/jdufner/webscraper/picturechoice/PictureSelectorTest.java
+++ src/test/java/de/jdufner/webscraper/picturechoice/PictureSelectorTest.java
@@ -1,14 +1,25 @@
 package de.jdufner.webscraper.picturechoice;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PictureSelectorTest {
+
+    @Mock
+    private PictureRepository pictureRepository;
+
+    @InjectMocks
+    private PictureSelector pictureSelector;
 
     @Test
     void given_interval_when_call_thousand_times_expect_min_and_max() {
@@ -24,9 +35,44 @@ class PictureSelectorTest {
         }
 
         // assert
-        Assertions.assertThat(count[0]).isEqualTo(0);
-        Assertions.assertThat(count[1]).isGreaterThanOrEqualTo(1);
-        Assertions.assertThat(count[10]).isGreaterThanOrEqualTo(1);
+        assertThat(count[0]).isEqualTo(0);
+        assertThat(count[1]).isGreaterThanOrEqualTo(1);
+        assertThat(count[10]).isGreaterThanOrEqualTo(1);
     }
 
+    @Test
+    void given_interval_and_exclude_when_call_thousand_times_expect_min_and_max() {
+        // arrange
+        int min = 1;
+        int max = 10;
+        int exclude = 3;
+        int[] count = new int[max + 1];
+
+        // act
+        for (int i = 0; i < 1_000; i++) {
+            int random = PictureSelector.randInt(min, max, exclude);
+            count[random]++;
+        }
+
+        // assert
+        assertThat(count[0]).isEqualTo(0);
+        assertThat(count[min]).isGreaterThanOrEqualTo(1);
+        assertThat(count[exclude]).isEqualTo(0);
+        assertThat(count[max]).isGreaterThanOrEqualTo(1);
+    }
+
+    @Test
+    void given_table_when_select_then_return_random_pictures() {
+        // arrange
+        when(pictureRepository.totalNumberPictures()).thenReturn(10);
+        when(pictureRepository.loadPictureOrNextAfter(anyInt()))
+                .thenReturn(new Picture(Path.of("1"), "1", Picture.State.INITIALIZED))
+                .thenReturn(new Picture(Path.of("2"), "2", Picture.State.INITIALIZED));
+
+        // act
+        Picture[] pictures = pictureSelector.selectTwoRandomPictures();
+
+        // assert
+        assertThat(pictures.length).isEqualTo(2);
+    }
 }
